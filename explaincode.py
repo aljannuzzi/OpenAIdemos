7cf61055d1c9815667696e00bc248562430dc14a
Alessandro Jannuzzi <alessandro.jannuzzi@microsoft.com>
2023-04-04 19:39:43 -0300

    Update explaincode.py

--- explaincode.py
+++ explaincode.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-# Get the OpenAI API key from a file
+# Get the OpenAI API key from a file test2
 with open("./inputs/inputs.txt", "r") as f:
     inputs = dict(line.strip().split(":") for line in f)
 openai_api_key = inputs["openai_api_key"]
